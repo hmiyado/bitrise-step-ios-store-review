3e8e81208a7f7eafe7c7ae4490dd7137216a9a6c
miyado <hmiyado@gmail.com>
2021-06-27 15:57:43 +0900

    extract func fetchFeed

--- main.go
+++ main.go
@@ -23,6 +23,34 @@ type Entry struct {
 
 func main() {
 	appId := os.Getenv("ios_app_id")
+	feed := fetchFeed(appId)
+	fmt.Printf("count of entries: %d", len(feed.Entries))
+	for _, entry := range feed.Entries {
+		fmt.Printf("[%s] %s\n",entry.Updated, entry.Title)
+	}
+
+	//
+	// --- Step Outputs: Export Environment Variables for other Steps:
+	// You can export Environment Variables for other Steps with
+	//  envman, which is automatically installed by `bitrise setup`.
+	// A very simple example:
+	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	if err != nil {
+		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
+		os.Exit(1)
+	}
+	// You can find more usage examples on envman's GitHub page
+	//  at: https://github.com/bitrise-io/envman
+
+	//
+	// --- Exit codes:
+	// The exit code of your Step is very important. If you return
+	//  with a 0 exit code `bitrise` will register your Step as "successful".
+	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	os.Exit(0)
+}
+
+func fetchFeed(appId string) Feed {
 	appUrl := "https://itunes.apple.com/jp/rss/customerreviews/page=1/id=" + appId + "/sortby=mostrecent/xml?urlDesc=/customerreviews/id=" + appId + "/sortBy=mostRecent/json"
 
 	response, err := http.Get(appUrl)
@@ -49,30 +77,5 @@ func main() {
 		fmt.Printf("Fialed to parse xml of store review,\n%s\n", string(body))
 		os.Exit(1)
 	}
-	fmt.Printf("url: %s", appUrl)
-	fmt.Printf("body: %s", string(body))
-	fmt.Printf("count of entries: %d", len(feed.Entries))
-	for _, entry := range feed.Entries {
-		fmt.Printf("[%s] %s\n",entry.Updated, entry.Title)
-	}
-
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
-	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
-	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
-
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
-	os.Exit(0)
+	return feed
 }
