cd462a1e375594aa9336ebdf6d265931fa5e5885
miyado <hmiyado@gmail.com>
2021-07-04 14:50:33 +0900

    extract feed.go

--- main.go
+++ main.go
@@ -2,36 +2,11 @@ package main
 
 import (
     "bytes"
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
-	"time"
 )
-
-type Feed struct {
-	XMLNAME xml.Name `xml:"feed"`
-	Entries []Entry `xml:"entry"`
-}
-
-type Entry struct {
-	XMLNAME xml.Name `xml:"entry"`
-	Updated time.Time `xml:"updated"`
-	Title string `xml:"title"`
-	Content []Content `xml:"content"` 
-	Rating int `xml:"rating"`
-	Version string `xml:"version"`
-	Author string `xml:"author>name"`
-}
-
-type Content struct {
-	XMLNAME xml.Name `xml:"content"`
-	Type string `xml:"type,attr"`
-	Body string `xml:",innerxml"`
-}
 
 func main() {
 	appId := os.Getenv("ios_app_id")
@@ -52,52 +27,6 @@ func main() {
 	//  with a 0 exit code `bitrise` will register your Step as "successful".
 	// Any non zero exit code will be registered as "failed" by `bitrise`.
 	os.Exit(0)
-}
-
-func FetchFeed(appId string) Feed {
-	appUrl := "https://itunes.apple.com/jp/rss/customerreviews/page=1/id=" + appId + "/sortby=mostrecent/xml?urlDesc=/customerreviews/id=" + appId + "/sortBy=mostRecent/json"
-
-	response, err := http.Get(appUrl)
-	defer response.Body.Close()
-	if err != nil {
-		fmt.Printf("Failed to get review from %s, error: %#v\n", appUrl, err)
-		os.Exit(1)
-	} 
-	responseStatus := string(response.Status)
-	if !strings.Contains(responseStatus, "200") {
-		fmt.Printf("Response from ios store is not OK but: %s", responseStatus)
-		os.Exit(1)
-	}
- 
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Printf("Failed to read response body from %s, error: %#v \n", appUrl, err)
-		os.Exit(1)
-	}
-
-	feed := Feed{}
-	err = xml.Unmarshal(body, &feed)
-	if err != nil {
-		fmt.Printf("Fialed to parse xml of store review,\n%s\n", string(body))
-		os.Exit(1)
-	}
-	return feed
-}
-
-func (e *Entry) ToString() string {
-	return fmt.Sprintf("[%s]<V:%s><R:%d> %s -- %s\n%s\n", e.Updated, e.Version, e.Rating, e.Title, e.Author, e.Content[0].Body)
-}
-
-func (f Feed) FilterEntriesByLastMinutes(lastMinutes int) []Entry {
-	entries := []Entry{}
-	lastValidTime := time.Now().Truncate(time.Duration(lastMinutes) * time.Minute)
-
-	for _, entry := range f.Entries {
-		if entry.Updated.After(lastValidTime) {
-			entries = append(entries, entry)
-		}
-	}
-	return entries
 }
 
 func PostToSlack(entry Entry, webhookUrl string) {
@@ -158,11 +87,4 @@ func (e *Entry) ToSlackPayloadJson() string {
 	}`
 	payload := fmt.Sprintf(payloadTemplate, e.Title, rating, e.Content[0].Body, authorAndDate)
 	return payload
-}
-
-func PrintEntries(entries []Entry) {
-	fmt.Printf("count of entries: %d\n", len(entries))
-	for _, entry := range entries {
-		fmt.Printf(entry.ToString())
-	}
 }
